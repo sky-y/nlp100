fb9442c24d84fd7dc147d8fcb4cbe64c8d3962a1
Yuki Fujiwara <sky.y.0079@gmail.com>
2021-05-22 13:19:04 +0900

    [modify] 16: about split commnad

--- chap2/16.py
+++ chap2/16.py
@@ -20,8 +20,12 @@ if len(sys.argv) < 2:
 n = int(sys.argv[1])
 
 
-
-# 注意：BSDのsplitには -n や -d オプションがない
+# コマンド
+# 注意：BSDのsplitには -n や -d オプションがない（GNUのみ）
+# → brew install coreutils
+# 参考 https://linuxjm.osdn.jp/info/GNU_coreutils/coreutils-ja_31.html
+#     https://yu8mada.com/2018/07/25/install-gnu-commands-on-macos-with-homebrew/
+# 
 # GNU: split -n l/3 -d popular-names.txt split-
 # macOS: gsplit -n l/3 -d popular-names.txt split-
 # → split-00, split-01, split-02
